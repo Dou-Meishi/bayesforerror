bb5990563c1bee00c574378aeadffd3c895ec67c
Dou Meishi <wangytnk@163.com>
2019-11-27 19:29:57 +0800

    up to part of TABLE. III.

---
+++ code/tab2.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Calculate table II
+
+# todo:
+# + round off rule as uncertainty
+
+import numpy as np
+from scipy.optimize import root_scalar
+
+def dkp_A_eps(p, *, Q, k, cbar_ge, cbar_k, err=.001):
+    '''Return the half width of p% DOB interval.'''
+    n_c = int(k)+1
+    p_t = n_c/(n_c+1) * cbar_k
+    p_t *= ((1/cbar_k)**(n_c+1)-(1/cbar_ge)**(n_c+1))
+    p_t /= ((1/cbar_k)**(n_c)-(1/cbar_ge)**(n_c))
+    if p <= p_t:
+        return cbar_k/p_t * p * Q**(k+1)
+    else:
+        def f(dkp):
+            y = 1/dkp**n_c - 1/(cbar_ge*Q**(k+1))**n_c
+            y *= Q**((k+1)*(n_c+1))/n_c
+            y -= (cbar_ge*Q**(k+1) - dkp)/cbar_ge**(n_c+1)
+            y *= cbar_ge**n_c*cbar_k**n_c/(cbar_ge**n_c - cbar_k**n_c)
+            y *= n_c/(n_c+1)/Q**(k+1)
+            return 1-y-p
+        return root_scalar(f, bracket=[cbar_k*Q**(k+1), cbar_ge*Q**(k+1)],
+                           method='brentq').root
+    
+def main():
+    args = [[.20, .33, .50],       # Q
+            [1000, 4., 2.],        # cbar_ge
+            [0, 1, 2, 3, 4],       # k
+            [1]]                   # cbar_k
+    # get the Cartesian product of args
+    args = np.stack(np.meshgrid(*args),axis=-1).reshape(-1,len(args))
+    keys = ['Q','cbar_ge','k','cbar_k']
+
+    print('.68% DOB width:')
+    for p in [.68,.95]:
+        print('{:.0%} DOB width:'.format(p))
+        for arg in args:
+            kw = dict(zip(keys,arg))
+            d = dkp_A_eps(.68,**kw)
+            print('\t{:3g}'.format(d))
+
+
+if __name__ == '__main__':
+    main()
---
+++ code/tab3-1.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# Calculate table III
+
+import numpy as np
+from scipy.integrate import quad, nquad
+from scipy.optimize import root_scalar
+
+k = 2
+n_c = 3
+cbar_le = .001
+cbar_ge = 1000
+limit = 50
+
+def find_width(p, pr, *, bound=1):
+    '''
+    Find a interval (-d,+d) on which the integral of pr(x) equals p.
+    Assuming the p.d.f. pr(x) is symmetric about x=0.
+
+    Return d.
+    '''
+    integral = lambda d: 2*quad(pr, 0, d)[0]
+    t, max_t = (0, 1000)
+    while p >= integral(bound) and t < max_t:
+        t += 1
+        bound *= 10
+    if p < integral(bound):
+        f = lambda d: p-integral(d)
+        return root_scalar(f, bracket=[0, bound], method='brentq').root
+    else:
+        raise Exception('Upper bound of interval width not found!\
+        \nThe last tried interval is (-{d},+{d}) on which the integral\
+        is {prb} while given probability is {p}'.format(
+            d=bound, prb=integral(bound), p=p))
+
+def _dkp_eps(p, pr_cn_if_cbar, pr_cbar, *, Q, cns, limit=50):
+    def pr_delta(delta):
+        def numerator_f(cbar):
+            y = pr_cn_if_cbar(delta/Q**(k+1), cbar)
+            for cn in cns:
+                y *= pr_cn_if_cbar(cn, cbar)
+            return y*pr_cbar(cbar)
+        def denominator_f(cbar):
+            y = Q**(k+1)
+            for cn in cns:
+                y *= pr_cn_if_cbar(cn, cbar)
+            return y*pr_cbar(cbar)
+        numerator, _ = quad(numerator_f, 0, np.inf, limit=limit)
+        denominator, _ = quad(denominator_f, 0, np.inf, limit=limit)
+        return numerator/denominator
+    return find_width(p, pr_delta, bound=Q**(k+1))
+
+
+def pr_cn_if_cbar(cn, cbar, *, Set='C'):
+    if Set == 'C':
+        return np.exp(-1/2 * cn**2/cbar**2)/np.sqrt(2*np.pi)/cbar
+    else:
+        raise Exception('No priors given! Please select a set.')
+
+def pr_cbar(cbar, *, Set='C'):
+    if Set == 'C':
+        theta = lambda x:np.heaviside(x,0)
+        return 1/cbar/np.log(cbar_ge/cbar_le) * theta(
+            cbar-cbar_le)* theta(cbar_ge-cbar)
+    else:
+        raise Exception('No priors given! Please select a set.')
+
+
+def main():
+    for cns in [[1., 1., 1.], [1., .5, .1], [1., .1, .1]]:
+        print('cns is [{:.2f},{:.2f},{:.2f}]:'.format(*cns))
+        for p in [.68, .95]:
+            print('\tDOB is {:.2%}:'.format(p))
+            for Q in [.20, .33, .50]:
+                print('\t\tQ is {:.2f}:'.format(Q))
+                i3 = _dkp_eps(p, pr_cn_if_cbar, pr_cbar, Q=Q, cns=cns)
+                print('{:.3g}'.format(i3))
+
+
+if __name__ == '__main__':
+    main()
